25cd8ba1377e02a1bd9ecd60aa542ac6a8f18c20
rasaljaman <rasaljaman15@gmail.com>
2025-12-03 20:18:42 +0530

    Update feed to masonry layout

--- components/GalleryGrid.tsx
+++ components/GalleryGrid.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
+import Masonry from 'react-masonry-css';
 import { ImageItem } from '../types';
 import ImageCard from './ImageCard';
+import './masonry.css'; 
 
 interface GalleryGridProps {
   images: ImageItem[];
 }
 
 const GalleryGrid: React.FC<GalleryGridProps> = ({ images }) => {
+  
+  const breakpointColumnsObj = {
+    default: 4,    // 4 columns on large screens
+    1100: 3,       // 3 columns on standard laptops
+    700: 2,        // 2 columns on tablets/large phones
+    500: 1         // 1 column on mobile
+  };
+
   if (images.length === 0) {
     return (
-      <div className="flex flex-col items-center justify-center py-20 text-textSecondary">
+      <div className="flex flex-col items-center justify-center py-20 text-gray-500">
         <p>No images found.</p>
       </div>
     );
   }
 
   return (
-    <div className="columns-2 md:columns-3 lg:columns-4 gap-4 px-4 pb-10">
-      {images.map((image) => (
-        <ImageCard key={image.id} image={image} />
-      ))}
+    <div className="px-4 pb-10">
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </Masonry>
     </div>
   );
 };
